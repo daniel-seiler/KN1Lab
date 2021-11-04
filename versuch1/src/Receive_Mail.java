03e97ec182c74622be6de10023dec26882bb4e90
Claudio Merz <claudio_merz@web.de>
2021-11-04 07:34:52 +0100

    Implemented exercise 3 from assignment 1

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -8,12 +8,36 @@ public class Receive_Mail {
 	public static void main(String[] args) throws Exception {
 		fetchMail();
 	}
-	
+
+	static Session session;
+
 	public static void fetchMail() {
 		try {
-			// your code here
-		} catch(Exception e) {
+			Properties props = System.getProperties();
+			props.put("mail.pop3.host", "localhost");
+			props.put("mail.store.protocol", "pop3");
+			session = Session.getInstance(props);
+
+			Store sessionStore = session.getStore();
+			sessionStore.connect("localhost", "labrat", "kn1lab");
+
+			Folder inbox = sessionStore.getFolder("INBOX");
+			inbox.open(1);
+			Message[] messages = inbox.getMessages();
+
+			for (Message m : messages) {
+				for (Address a : m.getFrom()) {
+					System.out.println("From: " + a.toString());
+				}
+				for (Address a : m.getAllRecipients()) {
+					System.out.println("To: " + a.toString());
+				}
+				System.out.println("Sent on: " + m.getSentDate().toString());
+				System.out.println("Subject: " + m.getSubject());
+				System.out.println(m.getContent());
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+	}	
 }
