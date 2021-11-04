c4d2a568d20496077ab865c3f55dea1a17c9d31d
daniel-seiler <73987329+daniel-seiler@users.noreply.github.com>
2021-11-04 10:39:52 +0100

    created function to send example mail via java mail api (#2)

--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,11 +1,10 @@
+import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage; 
+import javax.mail.internet.MimeMessage;
 
 public class Send_Mail {
 	public static void main(String[] args) {
@@ -14,7 +13,18 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			// your code here
+			Properties props = System.getProperties();
+			props.put("mail.smtp.host", "localhost");
+			Session session = Session.getInstance(props);
+
+			Message msg = new MimeMessage(session);
+			msg.setSubject("First mail subject");
+			msg.setSentDate(new Date());
+			msg.setFrom(new InternetAddress("sender@localhost"));
+			msg.setRecipient(Message.RecipientType.TO, new InternetAddress("labrat@localhost"));
+			msg.setText("First mail text");
+
+			Transport.send(msg);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
